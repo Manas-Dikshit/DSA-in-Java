84e41ce56847968e21c989f08854b34ed7dae314
Manas Ranjan Dikshit <manasdikshit48@gmail.com>
2025-08-19 15:04:30 +0530

    Add files via upload

---
+++ DataStructures/src/main/java/org/example/Fibonacci.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(50));
+    }
+    static int fibo(int n) {
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
---
+++ DataStructures/src/main/java/org/example/bsrc.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class bsrc {
+    public static void main(String[] args) {
+
+    }
+}
